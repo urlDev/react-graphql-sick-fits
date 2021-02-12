8727b06c49049f73534d664895d18162078adfa9
Can Ural <can.ural@outlook.com>
2021-02-12 22:07:33 +0200

    Added global styles and created css variables. Began making styled components with the header. Added NProgress progress bar for when the page is changing, there will be a custom styled progress bar shown that is hooked up to next js router. Fixed a bug about styles glitch with adding a server side rendering.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,18 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
+// NProgress helps us show a progress/loading bar when page loads
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+// Applying custom nprogress styles
+import '../components/styles/nprogress.css';
+
+// Hooking NProgress with next js router. NProgress will know
+// when to begin showing the bar and when to end it
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 // This component is to control the pages
 // Component and pageProps are for NextJs to understand
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,10 +1,27 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
+  // This is for serverSideRendering.
+  // Apparently Wes copied it from docs
+  // (he says so)
+  // So we are doing the same
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en">
-        {/* <Head></Head>  */}
+        <Head />
         <body>
           <Main />
           <NextScript />
